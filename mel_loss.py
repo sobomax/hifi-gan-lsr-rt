a8d1b5105770cdc2e38aea8961e1f631a4a8d660
Maksym Sobolyev <sobomax@sippysoft.com>
2023-09-13 16:26:03 -0700

    Make phase component optional for now.

--- mel_loss.py
+++ mel_loss.py
@@ -20,6 +20,7 @@ class MeanFilter(torch.nn.Module):
 
 class MelLossAP():
     phase_filter: MeanFilter
+    include_phase = False
 
     def __init__(self, device):
         self.phase_filter = MeanFilter(device, kernel_size=5)
@@ -36,19 +37,21 @@ class MelLossAP():
         for arg in args:
             assert self.phase_filter.conv.weight.data.device == arg.device
         y_mel_a, y_g_hat_mel_a, y_mel_p, y_g_hat_mel_p = args
-        loss_phase = y_mel_p - y_g_hat_mel_p
-        #print(1, loss_phase.size())
-        loss_phase = torch.sin(loss_phase / 2)
-        #print(2, loss_phase.size())
-        loss_phase = self.phase_filter(loss_phase.unsqueeze(1)).squeeze(1)
-        #print(3, loss_phase.size())
-        loss_phase = loss_phase.abs()
         y_mel_a, y_g_hat_mel_a = self.norm_mels(y_mel_a,
-            y_g_hat_mel_a)
-        max_ampl = torch.max(y_mel_a, y_g_hat_mel_a)
-        loss_phase = loss_phase * max_ampl
-        assert loss_phase.min() >= 0.0 and loss_phase.max() <= 1.0
-        loss_mel = torch.abs(y_mel_a - y_g_hat_mel_a) + loss_phase
+                y_g_hat_mel_a)
+        loss_mel = torch.abs(y_mel_a - y_g_hat_mel_a)
+        if self.include_phase:
+            loss_phase = y_mel_p - y_g_hat_mel_p
+            #print(1, loss_phase.size())
+            loss_phase = torch.sin(loss_phase / 2)
+            #print(2, loss_phase.size())
+            loss_phase = self.phase_filter(loss_phase.unsqueeze(1)).squeeze(1)
+            #print(3, loss_phase.size())
+            loss_phase = loss_phase.abs()
+            max_ampl = torch.max(y_mel_a, y_g_hat_mel_a)
+            loss_phase = loss_phase * max_ampl
+            assert loss_phase.min() >= 0.0 and loss_phase.max() <= 1.0
+            loss_mel = loss_mel + loss_phase
         loss_freq = loss_mel.sum(dim=1).mean()
         loss_time = loss_mel.sum(dim=2).mean()
         loss_mel = (0.5 * loss_freq + 0.5 * loss_time)
